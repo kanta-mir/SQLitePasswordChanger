7f9c8537bf37ebfddf4c59dc601eb6928d93d1ac
saleem.meer <kanta_mir@yahoo.com>
2021-03-30 18:08:08 +0200

    First commit sqlite password changer
    First commit sqlite password changer

---
+++ ChangeSqlitePassword/frmMain.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ChangeSqlitePassword
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSourceDbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectDb = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.txtOldPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSourceDbPath
+            // 
+            this.txtSourceDbPath.Location = new System.Drawing.Point(94, 61);
+            this.txtSourceDbPath.Name = "txtSourceDbPath";
+            this.txtSourceDbPath.Size = new System.Drawing.Size(568, 20);
+            this.txtSourceDbPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source DB:";
+            // 
+            // btnSelectDb
+            // 
+            this.btnSelectDb.Location = new System.Drawing.Point(678, 59);
+            this.btnSelectDb.Name = "btnSelectDb";
+            this.btnSelectDb.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectDb.TabIndex = 2;
+            this.btnSelectDb.Text = "Select";
+            this.btnSelectDb.UseVisualStyleBackColor = true;
+            this.btnSelectDb.Click += new System.EventHandler(this.btnSelectDb_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(678, 115);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 62);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(597, 115);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 62);
+            this.btnChange.TabIndex = 4;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // txtOldPassword
+            // 
+            this.txtOldPassword.Location = new System.Drawing.Point(94, 98);
+            this.txtOldPassword.Name = "txtOldPassword";
+            this.txtOldPassword.Size = new System.Drawing.Size(356, 20);
+            this.txtOldPassword.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Old Password:";
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Location = new System.Drawing.Point(94, 137);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.Size = new System.Drawing.Size(356, 20);
+            this.txtNewPassword.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "New Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(94, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(358, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Note: Leave the New Password Empty to remove the password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 18F);
+            this.label5.Location = new System.Drawing.Point(94, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(286, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "SQLite Password Changer";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 194);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOldPassword);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSelectDb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSourceDbPath);
+            this.Name = "frmMain";
+            this.Text = "SQLite Password Changer 0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSourceDbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectDb;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox txtOldPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ChangeSqlitePassword/frmMain.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeSqlitePassword
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectDb_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "sqlite db (*.db)|*.db|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    txtSourceDbPath.Text = filePath;
+
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            var sourceDb = txtSourceDbPath.Text.Trim();
+            var oldPassword = txtOldPassword.Text.Trim();
+            var connstr = "Data Source=" + sourceDb + ";Version=3;New=False;Compress=True;Password=" + oldPassword;
+            using (SQLiteConnection sQLiteConnection = new SQLiteConnection(connstr))
+            {
+                sQLiteConnection.Open();
+                var newPass = txtNewPassword.Text.Trim();
+                 sQLiteConnection.ChangePassword(newPass);
+                var query = "select * from [table-name]";
+                SQLiteCommand sQLiteCommand = sQLiteConnection.CreateCommand();
+                sQLiteCommand.CommandText = query;
+                sQLiteCommand.ExecuteNonQuery();
+
+                SQLiteDataReader dr = sQLiteCommand.ExecuteReader();
+                while (dr.Read())
+                {
+                    MessageBox.Show(dr["column-name"].ToString());
+                }
+            }
+        }
+    }
+}
